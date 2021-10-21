341c9c0728253d4349704ac8911049b14b6381d2
Oleg Derevyanko <trekol39042@gmail.com>
2021-10-21 23:40:21 +0300

    #334. Remove UIKit files.

--- ios/Codeforces Watcher/Application/features/users/UsersViewController.swift
+++ ios/Codeforces Watcher/Application/features/users/UsersViewController.swift
@@ -306,7 +306,7 @@ class UsersViewController: UIViewControllerWithFab, ReKampStoreSubscriber {
                 subtitle: "verify_account_prompt".localized,
                 buttonText: "verify_in_42_seconds".localized,
                 onButtonTap: {
-                    self.presentModal(VerifyViewControllerNew())
+                    self.presentModal(VerifyViewController())
                     analyticsControler.logEvent(eventName: AnalyticsEvents().VERIFY_OPENED, params: [:])
                 }
             )
