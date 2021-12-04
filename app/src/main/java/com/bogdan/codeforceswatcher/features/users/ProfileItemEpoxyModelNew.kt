b48c44d92e646b7b5ff463e53d2081d2476b6d96
Ilya Kerymov <illya.kerymov31@gmail.com>
2021-12-04 19:20:01 +0200

    #360. Add LastUpdate view.

--- app/src/main/java/com/bogdan/codeforceswatcher/features/users/ProfileItemEpoxyModelNew.kt
+++ app/src/main/java/com/bogdan/codeforceswatcher/features/users/ProfileItemEpoxyModelNew.kt
@@ -3,11 +3,14 @@ package com.bogdan.codeforceswatcher.features.users
 import android.content.Context
 import android.content.Intent
 import android.view.View
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.bogdan.codeforceswatcher.R
 import com.bogdan.codeforceswatcher.components.compose.theme.AlgoismeTheme
@@ -21,12 +24,14 @@ import io.xorum.codeforceswatcher.features.auth.models.UserAccount
 import io.xorum.codeforceswatcher.features.auth.redux.AuthState
 import io.xorum.codeforceswatcher.redux.analyticsController
 import io.xorum.codeforceswatcher.util.AnalyticsEvents
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ProfileItemEpoxyModel(
     private val userAccount: UserAccount?,
     private val authStage: AuthState.Stage,
 
-) : BaseEpoxyModel(R.layout.view_compose) {
+    ) : BaseEpoxyModel(R.layout.view_compose) {
 
     init {
         id("ProfileItemEpoxyModel", userAccount.toString(), authStage.toString())
@@ -43,21 +48,49 @@ class ProfileItemEpoxyModel(
     }
 
     @Composable
-    private fun ProfileItem(context: Context) {
+    private fun ProfileItem(context: Context, modifier: Modifier = Modifier) {
         when (authStage) {
             AuthState.Stage.NOT_SIGNED_IN -> {
-                IdentifyView(Modifier.padding(20.dp)) { startSignInActivity(context) }
+                IdentifyView(modifier.padding(20.dp)) { startSignInActivity(context) }
             }
             AuthState.Stage.SIGNED_IN -> {
-                VerifyView(Modifier.padding(20.dp)) { startVerifyActivity(context) }
+                VerifyView(modifier.padding(20.dp)) { startVerifyActivity(context) }
             }
             AuthState.Stage.VERIFIED -> {
-                ProfileView(userAccount?.codeforcesUser!!, Modifier.padding(20.dp)) {
-                    startUserAccountActivity(context)
+                Column(modifier.padding(20.dp)) {
+                    ProfileView(userAccount?.codeforcesUser!!) { startUserAccountActivity(context) }
+
+                    Spacer(Modifier.height(6.dp))
+
+                    LastUpdate(context, Modifier.fillMaxWidth())
                 }
             }
         }
     }
+
+    @Composable
+    private fun LastUpdate(context: Context, modifier: Modifier = Modifier) {
+        Text(
+            text = buildLastUpdate(context),
+            style = MaterialTheme.typography.body1,
+            color = MaterialTheme.colors.secondaryVariant,
+            textAlign = TextAlign.Center,
+            modifier = modifier
+        )
+    }
+
+    private fun buildLastUpdate(context: Context) =
+        userAccount?.codeforcesUser?.ratingChanges?.lastOrNull()?.let { ratingChange ->
+            context.getString(
+                R.string.updated_on,
+                SimpleDateFormat(
+                    context.getString(R.string.user_date_format),
+                    Locale.getDefault()
+                ).format(
+                    Date(ratingChange.ratingUpdateTimeSeconds * 1000)
+                )
+            )
+        } ?: context.getString(R.string.never_updated)
 
     private fun startSignInActivity(context: Context) {
         context.startActivity(Intent(context, SignInComposeActivity::class.java))
