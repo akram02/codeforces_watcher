2212a5f8f69b6e7e657e72a7acdab310f9c0dffc
Oleg Derevyanko <trekol39042@gmail.com>
2022-01-04 13:52:22 +0200

    #370; Split SearchBarView into private components.

--- ios/Codeforces Watcher/Application/features/problems/SearchBarView.swift
+++ ios/Codeforces Watcher/Application/features/problems/SearchBarView.swift
@@ -16,70 +16,95 @@ struct SearchBarView: View {
     var body: some View {
         HStack(spacing: 20) {
             if !isShownSearchField {
-                CommonText("Problems".localized)
-                    .font(.headerMedium)
-                    .foregroundColor(Palette.black.swiftUIColor)
-                    .frame(maxWidth: .infinity, alignment: .leading)
+                TitleBar
             }
             
             ZStack(alignment: .trailing) {
-                SearchTextField(
-                    text: $searchBoxText,
-                    placeholder: didShownSearchField ? "Search for problems...".localized : "",
-                    willShow: $willShowSearchField,
-                    willHide: $willHideSearchField
-                )
-                    .frame(maxWidth: isShownSearchField ? .infinity : 30)
-                    .frame(height: 30)
+                SearchTextFieldView
                 
-                Button(action: {
-                    withAnimation(.easeIn(duration: textFieldAnimationDuration)) {
-                        willShowSearchField.toggle()
-                        isShownSearchField.toggle()
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                            willShowSearchField.toggle()
-                        }
-                    }
-                    DispatchQueue.main.asyncAfter(
-                        deadline: .now() + textFieldAnimationDuration + placehoderAnimationDelay
-                    ) {
-                        didShownSearchField.toggle()
-                    }
-                }, label: {
-                    Image("searchIcon")
-                        .renderingMode(.original)
-                })
-                    .disabled(isShownSearchField)
+                SearchIconView
             }
             
             if isShownSearchField {
-                Button(action: {
-                    didShownSearchField.toggle()
-                    DispatchQueue.main.asyncAfter(deadline: .now() + placehoderAnimationDelay) {
-                        withAnimation(.easeIn(duration: textFieldAnimationDuration)) {
-                            willHideSearchField.toggle()
-                            isShownSearchField.toggle()
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                                willHideSearchField.toggle()
-                            }
-                        }
-                    }
-                }, label: {
-                    Image("crossIconNew")
-                        .renderingMode(.original)
-                })
+                CrossIconView
             } else {
-                Button(action: {
-                    onFilter()
-                }, label: {
-                    Image("filterIcon")
-                        .renderingMode(.original)
-                })
+                FilterIconView
             }
         }
         .frame(maxWidth: .infinity)
         .frame(height: 56, alignment: .center)
         .padding(.horizontal, 20)
+    }
+    
+    @ViewBuilder
+    private var TitleBar: some View {
+        CommonText("Problems".localized)
+            .font(.headerMedium)
+            .foregroundColor(Palette.black.swiftUIColor)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    @ViewBuilder
+    private var SearchTextFieldView: some View {
+        SearchTextField(
+            text: $searchBoxText,
+            placeholder: didShownSearchField ? "Search for problems...".localized : "",
+            willShow: $willShowSearchField,
+            willHide: $willHideSearchField
+        )
+            .frame(maxWidth: isShownSearchField ? .infinity : 30)
+            .frame(height: 30)
+    }
+    
+    @ViewBuilder
+    private var SearchIconView: some View {
+        Button(action: {
+            withAnimation(.easeIn(duration: textFieldAnimationDuration)) {
+                willShowSearchField.toggle()
+                isShownSearchField.toggle()
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    willShowSearchField.toggle()
+                }
+            }
+            DispatchQueue.main.asyncAfter(
+                deadline: .now() + textFieldAnimationDuration + placehoderAnimationDelay
+            ) {
+                didShownSearchField.toggle()
+            }
+        }, label: {
+            Image("searchIcon")
+                .renderingMode(.original)
+        })
+            .disabled(isShownSearchField)
+    }
+    
+    @ViewBuilder
+    private var FilterIconView: some View {
+        Button(action: {
+            onFilter()
+        }, label: {
+            Image("filterIcon")
+                .renderingMode(.original)
+        })
+    }
+    
+    @ViewBuilder
+    private var CrossIconView: some View {
+        Button(action: {
+            didShownSearchField.toggle()
+            DispatchQueue.main.asyncAfter(deadline: .now() + placehoderAnimationDelay) {
+                withAnimation(.easeIn(duration: textFieldAnimationDuration)) {
+                    willHideSearchField.toggle()
+                    isShownSearchField.toggle()
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                        willHideSearchField.toggle()
+                    }
+                }
+            }
+        }, label: {
+            Image("crossIconNew")
+                .renderingMode(.original)
+        })
     }
 }
 
