a51df05bd5e0660491d24812a763a42e0dc921f6
Oleg Derevyanko <trekol39042@gmail.com>
2022-02-10 16:12:59 +0200

    #371; Extract composable to one file with fragment.

--- app/src/main/java/com/bogdan/codeforceswatcher/features/problems/ProblemsFragmentNew.kt
+++ app/src/main/java/com/bogdan/codeforceswatcher/features/problems/ProblemsFragmentNew.kt
@@ -11,8 +11,26 @@ import io.xorum.codeforceswatcher.redux.store
 import tw.geothings.rekotlin.StoreSubscriber
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.bogdan.codeforceswatcher.components.WebViewActivity
 import io.xorum.codeforceswatcher.util.AnalyticsEvents
+import com.bogdan.codeforceswatcher.R
 
 class ProblemsFragmentNew : Fragment(), StoreSubscriber<ProblemsState> {
 
@@ -76,3 +94,79 @@ class ProblemsFragmentNew : Fragment(), StoreSubscriber<ProblemsState> {
         )
     }
 }
+
+@Composable
+private fun ProblemsView(
+    problemsState: State<List<ProblemUIModel>>,
+    onProblem: (String, String) -> Unit
+) {
+    Box {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colors.primaryVariant)
+        )
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(RoundedCornerShape(30.dp, 30.dp, 0.dp, 0.dp))
+                .background(MaterialTheme.colors.primary)
+        ) {
+            items(problemsState.value) { problem ->
+                ProblemView(
+                    problem = problem,
+                    onProblem = onProblem
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun ProblemView(
+    problem: ProblemUIModel,
+    onProblem: (String, String) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp, 20.dp, 20.dp, 0.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(
+            modifier = Modifier.clickable { onProblem(problem.link, problem.title) }
+        ) {
+            Text(
+                text = problem.title,
+                style = MaterialTheme.typography.subtitle2,
+                color = MaterialTheme.colors.secondary,
+                modifier = Modifier.height(20.dp)
+            )
+
+            Text(
+                text = problem.subtitle,
+                style = MaterialTheme.typography.body1,
+                color = MaterialTheme.colors.secondaryVariant,
+                modifier = Modifier.height(16.dp)
+            )
+        }
+
+        Icon(
+            painter = painterResource(R.drawable.ic_star),
+            contentDescription = null,
+            tint = colorResource(if (problem.isFavourite) R.color.colorAccent else R.color.black),
+            modifier = Modifier
+                .size(24.dp)
+        )
+    }
+}
+
+private data class ProblemUIModel(
+    val id: String,
+    val title: String,
+    val subtitle: String,
+    val link: String,
+    val isFavourite: Boolean
+)
