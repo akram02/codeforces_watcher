169d842bcf246b0f154b19456e83e202d6ca73ef
Oleg Derevyanko <trekol39042@gmail.com>
2022-02-10 16:07:51 +0200

    #371; Display real data.

--- app/src/main/java/com/bogdan/codeforceswatcher/features/problems/ProblemsFragmentNew.kt
+++ app/src/main/java/com/bogdan/codeforceswatcher/features/problems/ProblemsFragmentNew.kt
@@ -2,23 +2,77 @@ package com.bogdan.codeforceswatcher.features.problems
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import com.bogdan.codeforceswatcher.components.compose.theme.AlgoismeTheme
+import io.xorum.codeforceswatcher.features.problems.redux.ProblemsState
+import io.xorum.codeforceswatcher.redux.store
+import tw.geothings.rekotlin.StoreSubscriber
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import com.bogdan.codeforceswatcher.components.WebViewActivity
+import io.xorum.codeforceswatcher.util.AnalyticsEvents
 
-class ProblemsFragmentNew : Fragment() {
+class ProblemsFragmentNew : Fragment(), StoreSubscriber<ProblemsState> {
+
+    private var problemsState: MutableState<List<ProblemUIModel>> = mutableStateOf(emptyList())
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        return ComposeView(requireContext()).apply {
-            setContent {
-                AlgoismeTheme {
-                    ProblemsView()
-                }
+    ) = ComposeView(requireContext()).apply {
+        setContent {
+            AlgoismeTheme {
+                ProblemsView(
+                    problemsState = problemsState,
+                    onProblem = { link, title ->
+                        onProblem(link, title)
+                    }
+                )
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        store.subscribe(this) { state ->
+            state.skipRepeats { oldState, newState ->
+                oldState.problems.status == newState.problems.status
+                        && oldState.problems.isFavourite == newState.problems.isFavourite
+                        && oldState.problems.filteredProblems == newState.problems.filteredProblems
+            }.select { it.problems }
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        store.unsubscribe(this)
+    }
+
+    override fun onNewState(state: ProblemsState) {
+        problemsState.value = state.filteredProblems.map {
+            ProblemUIModel(
+                it.id,
+                it.title,
+                it.subtitle,
+                it.link,
+                it.isFavourite
+            )
+        }
+    }
+
+    private fun onProblem(link: String, title: String) {
+        startActivity(
+            WebViewActivity.newIntent(
+                requireContext(),
+                link,
+                title,
+                AnalyticsEvents.PROBLEM_OPENED,
+                AnalyticsEvents.PROBLEM_SHARED
+            )
+        )
     }
 }
--- app/src/main/java/com/bogdan/codeforceswatcher/features/problems/ProblemsView.kt
+++ app/src/main/java/com/bogdan/codeforceswatcher/features/problems/ProblemsView.kt
@@ -1,24 +1,29 @@
 package com.bogdan.codeforceswatcher.features.problems
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bogdan.codeforceswatcher.R
-import com.bogdan.codeforceswatcher.components.compose.theme.AlgoismeTheme
 
 @Composable
-fun ProblemsView() {
+fun ProblemsView(
+    problemsState: State<List<ProblemUIModel>>,
+    onProblem: (String, String) -> Unit
+) {
     Box {
         Box(
             modifier = Modifier
@@ -28,56 +33,64 @@ fun ProblemsView() {
 
         LazyColumn(
             modifier = Modifier
+                .fillMaxSize()
                 .clip(RoundedCornerShape(30.dp, 30.dp, 0.dp, 0.dp))
                 .background(MaterialTheme.colors.primary)
-                .padding(top = 20.dp)
         ) {
-            items(100) {
-                ProblemView()
+            items(problemsState.value) { problem ->
+                ProblemView(
+                    problem = problem,
+                    onProblem = onProblem
+                )
             }
         }
     }
 }
 
 @Composable
-private fun ProblemView() {
+private fun ProblemView(
+    problem: ProblemUIModel,
+    onProblem: (String, String) -> Unit
+) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 20.dp),
+            .padding(20.dp, 20.dp, 20.dp, 0.dp),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Column {
+        Column(
+            modifier = Modifier.clickable { onProblem(problem.link, problem.title) }
+        ) {
             Text(
-                text = "1264A: Feeding Chicken",
+                text = problem.title,
                 style = MaterialTheme.typography.subtitle2,
-                color = MaterialTheme.colors.secondary
+                color = MaterialTheme.colors.secondary,
+                modifier = Modifier.height(20.dp)
             )
 
             Text(
-                text = "Codeforces Round #601 (Div. 1)",
+                text = problem.subtitle,
                 style = MaterialTheme.typography.body1,
                 color = MaterialTheme.colors.secondaryVariant,
-                modifier = Modifier.offset(y = (-6).dp)
+                modifier = Modifier.height(16.dp)
             )
         }
 
         Icon(
             painter = painterResource(R.drawable.ic_star),
             contentDescription = null,
-            tint = MaterialTheme.colors.onBackground,
+            tint = colorResource(if (problem.isFavourite) R.color.colorAccent else R.color.black),
             modifier = Modifier
                 .size(24.dp)
-                .offset(y = (-6).dp)
         )
     }
 }
 
-@Preview
-@Composable
-private fun ComposablePreview() {
-    AlgoismeTheme {
-        ProblemsView()
-    }
-}
+data class ProblemUIModel(
+    val id: String,
+    val title: String,
+    val subtitle: String,
+    val link: String,
+    val isFavourite: Boolean
+)
