048b136f7efc3036b29c202e523648e58b4148ce
Oleg Derevyanko <trekol39042@gmail.com>
2022-02-16 14:53:28 +0200

    #373; Display real data.

--- app/src/main/java/com/bogdan/codeforceswatcher/features/problems/ProblemsFiltersActivityNew.kt
+++ app/src/main/java/com/bogdan/codeforceswatcher/features/problems/ProblemsFiltersActivityNew.kt
@@ -7,12 +7,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -21,23 +25,65 @@ import androidx.compose.ui.unit.dp
 import com.bogdan.codeforceswatcher.R
 import com.bogdan.codeforceswatcher.components.compose.theme.AlgoismeTheme
 import com.bogdan.codeforceswatcher.components.compose.NavigationBar
+import com.bogdan.codeforceswatcher.features.filters.models.FilterItem
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import io.xorum.codeforceswatcher.features.problems.redux.ProblemsRequests
+import io.xorum.codeforceswatcher.features.problems.redux.ProblemsState
+import io.xorum.codeforceswatcher.redux.store
+import tw.geothings.rekotlin.StoreSubscriber
 
-class ProblemsFiltersActivityNew : AppCompatActivity() {
+class ProblemsFiltersActivityNew : AppCompatActivity(), StoreSubscriber<ProblemsState> {
+
+    private val filtersState: MutableState<List<FilterItem>> = mutableStateOf(emptyList())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             AlgoismeTheme {
-                ProblemsFilters()
+                ProblemsFilters(
+                    filtersState = filtersState
+                )
             }
         }
     }
 
+    override fun onStart() {
+        super.onStart()
+
+        store.subscribe(this) { state ->
+            state.skipRepeats { oldState, newState ->
+                oldState.problems.tags == newState.problems.tags
+                        && oldState.problems.selectedTags == newState.problems.selectedTags
+            }.select { it.problems }
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        store.unsubscribe(this)
+    }
+
+    override fun onNewState(state: ProblemsState) {
+        filtersState.value = state.tags.toFilterItems(state)
+    }
+
+    private fun List<String>.toFilterItems(state: ProblemsState) = map { tag ->
+        FilterItem(
+            title = tag,
+            imageId = null,
+            isChecked = state.selectedTags.contains(tag),
+        ) { isChecked ->
+            store.dispatch(ProblemsRequests.ChangeTagCheckStatus(tag, isChecked))
+        }
+    }
+
     @Composable
-    private fun ProblemsFilters() {
+    private fun ProblemsFilters(
+        filtersState: State<List<FilterItem>>
+    ) {
         val swipeRefreshState = rememberSwipeRefreshState(false)
 
         Scaffold(
@@ -49,7 +95,7 @@ class ProblemsFiltersActivityNew : AppCompatActivity() {
                 state = swipeRefreshState,
                 onRefresh = { }
             ) {
-                FiltersList()
+                FiltersList(filtersState)
             }
         }
     }
@@ -62,37 +108,35 @@ class ProblemsFiltersActivityNew : AppCompatActivity() {
     )
 
     @Composable
-    private fun FiltersList() = LazyColumn(
+    private fun FiltersList(
+        filtersState: State<List<FilterItem>>
+    ) = LazyColumn(
         modifier = Modifier.fillMaxSize()
     ) {
-        items(100) {
-            FilterView()
+        items(filtersState.value) { filterItem ->
+            FilterView(filterItem)
         }
     }
 
     @Composable
-    private fun FilterView() = Row(
+    private fun FilterView(
+        filterItem: FilterItem
+    ) = Row(
         modifier = Modifier
             .fillMaxWidth()
             .padding(20.dp, 20.dp, 20.dp, 0.dp),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Text(
-            text = "dfs and similar",
+            text = filterItem.title,
             style = MaterialTheme.typography.subtitle1,
             color = MaterialTheme.colors.secondary
         )
 
         Icon(
-            painter = painterResource(id = R.drawable.ic_checkbox),
-            contentDescription = "Checkbox",
+            painter = painterResource(id = if (filterItem.isChecked) R.drawable.ic_checkbox else R.drawable.ic_checkbox_disabled),
+            contentDescription = null,
             modifier = Modifier.clickable { }
         )
-    }
-
-    @Preview
-    @Composable
-    private fun Preview() = AlgoismeTheme {
-        ProblemsFilters()
     }
 }
