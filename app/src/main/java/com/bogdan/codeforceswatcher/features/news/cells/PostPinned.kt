d36643bca3bc3fc154319c8fcf3e3579ab2cce14
Oleg Derevyanko <trekol39042@gmail.com>
2022-03-09 21:50:09 +0200

    #375; Add visible pinned post UI.

--- app/src/main/java/com/bogdan/codeforceswatcher/features/news/cells/PostPinned.kt
+++ app/src/main/java/com/bogdan/codeforceswatcher/features/news/cells/PostPinned.kt
@@ -3,12 +3,21 @@ package com.bogdan.codeforceswatcher.features.news.cells
 import android.graphics.PorterDuff
 import android.graphics.PorterDuffXfermode
 import android.graphics.Typeface
+import android.graphics.text.LineBreaker
+import android.os.Build
+import android.text.Layout
+import android.text.StaticLayout
+import android.text.TextDirectionHeuristics
+import android.text.TextPaint
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.Canvas
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
@@ -18,14 +27,19 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.bogdan.codeforceswatcher.R
+import com.bogdan.codeforceswatcher.components.compose.buttons.CommonButton
+import com.bogdan.codeforceswatcher.components.compose.buttons.SmallButton
 import com.bogdan.codeforceswatcher.components.compose.theme.AlgoismeTheme
 
 @Composable
 fun PostPinnedView() {
     val visibleState by remember { mutableStateOf(false) }
 
-    if (!visibleState) {
+    if (visibleState) {
+        VisibleView()
+    } else {
         HiddenView()
     }
 }
@@ -65,17 +79,25 @@ private fun HiddenView() = Box(
                 modifier = Modifier.size(32.dp)
             )
 
-            HiddenTitleView(modifier = Modifier.weight(1f))
+            HiddenTitleView()
         }
     }
 }
 
 @Composable
-private fun HiddenTitleView(modifier: Modifier) = Column(
-    modifier = modifier
+private fun HiddenTitleView() = Column(
+    modifier = Modifier
         .offset(y = (-4).dp)
 ) {
-    GradientText("Update 3.0: What’s New?")
+    GradientText(
+        "Update 3.0: What’s New?",
+        fontSize = 60f,
+        lineHeight = 68f,
+        origin = Offset(0f, 68f),
+        shaderFrom = Offset(0f, 0f),
+        shaderTo = Offset(800f, 0f),
+        modifier = Modifier.height(20.dp)
+    )
 
     Text(
         text = "Click here to see more",
@@ -87,34 +109,129 @@ private fun HiddenTitleView(modifier: Modifier) = Column(
 }
 
 @Composable
-private fun GradientText(name: String, modifier: Modifier = Modifier) {
+private fun VisibleView() = Box(
+    modifier = Modifier
+        .height(230.dp)
+        .clip(AlgoismeTheme.shapes.medium)
+) {
+    Image(
+        painter = painterResource(R.drawable.ic_pinned_post_background),
+        contentDescription = null,
+        modifier = Modifier.fillMaxWidth(),
+        contentScale = ContentScale.Crop
+    )
+
+    Box(modifier = Modifier.background(
+        brush = Brush.radialGradient(
+            0.42f to AlgoismeTheme.colors.black.copy(alpha = 0f),
+            0.88f to AlgoismeTheme.colors.black.copy(alpha = 0.6f),
+            center = Offset(x = 800f, y = 200f),
+            radius = 600f
+        )
+    )) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.ic_logo),
+                    contentDescription = null,
+                    modifier = Modifier.size(32.dp)
+                )
+
+                Image(
+                    painter = painterResource(R.drawable.ic_cross_icon),
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+
+            Spacer(modifier = Modifier.height(96.dp))
+
+            GradientText(
+                "Update 3.0:",
+                "What’s New?",
+                fontSize = 100f,
+                lineHeight = 110f,
+                origin = Offset(0f, 0f),
+                shaderFrom = Offset(0f, 40f),
+                shaderTo = Offset(520f, 0f),
+                modifier = Modifier.height(30.dp)
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.Bottom
+            ) {
+                Text(
+                    text = "Huge redesign, mentors & more!",
+                    style = AlgoismeTheme.typography.primarySemiBold,
+                    color = AlgoismeTheme.colors.white,
+                    modifier = Modifier.width(180.dp),
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+
+                CommonButton(
+                    label = "See details",
+                    modifier = Modifier.defaultMinSize(minWidth = 80.dp, minHeight = 32.dp),
+                    textStyle = AlgoismeTheme.typography.buttonSemiBold.copy(fontSize = 13.sp),
+                    backgroundColor = AlgoismeTheme.colors.primary,
+                    labelColor = AlgoismeTheme.colors.secondary
+                ) { }
+            }
+        }
+    }
+}
+
+@Composable
+private fun GradientText(
+    vararg names: String,
+    fontSize: Float,
+    lineHeight: Float,
+    origin: Offset,
+    shaderFrom: Offset,
+    shaderTo: Offset,
+    modifier: Modifier = Modifier
+) {
 
     val paint = Paint().asFrameworkPaint()
 
     val gradientShader: Shader = LinearGradientShader(
-        from = Offset(0f, 0f),
-        to = Offset(240f, 0f),
-        colors = listOf(AlgoismeTheme.colors.red, AlgoismeTheme.colors.yellow, AlgoismeTheme.colors.blue)
+        from = shaderFrom,
+        to = shaderTo,
+        colors = listOf(AlgoismeTheme.colors.red, AlgoismeTheme.colors.yellow, AlgoismeTheme.colors.blue),
+        colorStops = listOf(0f, 0.5f, 1f)
     )
 
-    Canvas(modifier.height(20.dp)) {
+    Canvas(modifier) {
         paint.apply {
             isAntiAlias = true
-            textSize = 60f
+            textSize = fontSize
             typeface = Typeface.create(Typeface.MONOSPACE, Typeface.BOLD)
             letterSpacing = -0.05f
             xfermode = PorterDuffXfermode(PorterDuff.Mode.DST_OVER)
         }
         drawIntoCanvas { canvas ->
-            canvas.save()
-            canvas.nativeCanvas.drawText(name, 0f, 68f, paint)
-            canvas.restore()
-            paint.shader = gradientShader
-            paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
-            paint.maskFilter = null
-            canvas.nativeCanvas.drawText(name, 0f, 68f, paint)
-            paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.OVERLAY)
-            canvas.nativeCanvas.drawText(name, 0f, 68f, paint)
+            names.forEachIndexed { index, name ->
+                canvas.save()
+                canvas.nativeCanvas.drawText(name, origin.x, origin.y + lineHeight * index, paint)
+                canvas.restore()
+                paint.shader = gradientShader
+                paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
+                paint.maskFilter = null
+                canvas.nativeCanvas.drawText(name, origin.x, origin.y + lineHeight * index, paint)
+                paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.OVERLAY)
+                canvas.nativeCanvas.drawText(name, origin.x, origin.y +lineHeight * index, paint)
+            }
         }
         paint.reset()
     }
