3eac5f78ceb31c3eb560fffb2cf77c9f38b69ca6
Oleg Derevyanko <trekol39042@gmail.com>
2022-03-13 02:07:36 +0200

    #366; Display real data.

--- app/src/main/java/com/bogdan/codeforceswatcher/features/contests/ContestsFragmentNew.kt
+++ app/src/main/java/com/bogdan/codeforceswatcher/features/contests/ContestsFragmentNew.kt
@@ -7,20 +7,34 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import com.bogdan.codeforceswatcher.R
 import com.bogdan.codeforceswatcher.components.compose.theme.AlgoismeTheme
+import io.xorum.codeforceswatcher.features.contests.models.Contest
+import io.xorum.codeforceswatcher.features.contests.redux.ContestsState
+import io.xorum.codeforceswatcher.redux.store
+import tw.geothings.rekotlin.StoreSubscriber
+import java.text.SimpleDateFormat
+import java.util.*
 
-class ContestsFragmentNew : Fragment() {
+class ContestsFragmentNew : Fragment(), StoreSubscriber<ContestsState> {
+
+    private val contestsState: MutableState<ContestsState?> = mutableStateOf(null)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,25 +42,58 @@ class ContestsFragmentNew : Fragment() {
     ) = ComposeView(requireContext()).apply {
         setContent {
             AlgoismeTheme {
-                ContentView()
+                ContentView(
+                    contestsState = contestsState
+                )
             }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        store.subscribe(this) { state ->
+            state.skipRepeats { oldState, newState ->
+                oldState.contests == newState.contests
+            }.select { it.contests }
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        store.unsubscribe(this)
+    }
+
+    override fun onNewState(state: ContestsState) {
+        contestsState.value = state
+    }
+}
+
+@Composable
+private fun ContentView(
+    contestsState: State<ContestsState?>
+) = Scaffold(
+    modifier = Modifier
+        .background(AlgoismeTheme.colors.primary)
+        .padding(horizontal = 20.dp)
+) {
+    val state = contestsState.value ?: return@Scaffold
+    val contests = state.contests.filter { it.phase == Contest.Phase.PENDING }
+        .sortedBy(Contest::startDateInMillis)
+        .filter { state.filters.contains(it.platform) }
+
+    LazyColumn {
+        items(contests) {
+            ContestView(it)
         }
     }
 }
 
 @Composable
-private fun ContentView() = LazyColumn(
-    modifier = Modifier
-        .background(AlgoismeTheme.colors.primary)
-        .padding(horizontal = 20.dp)
-) {
-    items(10) {
-        ContestView()
-    }
-}
-
-@Composable
-private fun ContestView() = Row(
+private fun ContestView(
+    contest: Contest
+) = Row(
     modifier = Modifier
         .fillMaxWidth()
         .padding(top = 20.dp)
@@ -54,15 +101,15 @@ private fun ContestView() = Row(
     verticalAlignment = Alignment.CenterVertically
 ) {
     Image(
-        painter = painterResource(R.drawable.ic_codeforces),
+        painter = painterResource(platformIcon(contest.platform)),
         contentDescription = null
     )
 
     Spacer(modifier = Modifier.width(8.dp))
 
-    Column {
+    Column(modifier = Modifier.fillMaxWidth(0.9f)) {
         Text(
-            text = "Codeforces Round #745 (Div. 1)",
+            text = contest.title,
             style = AlgoismeTheme.typography.primarySemiBold,
             color = AlgoismeTheme.colors.secondary,
             maxLines = 1,
@@ -70,7 +117,7 @@ private fun ContestView() = Row(
         )
 
         Text(
-            text = "13:05 Sep 30, Thursday",
+            text = getDateTime(contest.startDateInMillis),
             style = AlgoismeTheme.typography.hintRegular,
             color = AlgoismeTheme.colors.secondaryVariant,
             maxLines = 1,
@@ -87,6 +134,25 @@ private fun ContestView() = Row(
     )
 }
 
+private fun platformIcon(
+    platform: Contest.Platform
+) = when (platform) {
+    Contest.Platform.ATCODER -> R.drawable.ic_atcoder
+    Contest.Platform.TOPCODER -> R.drawable.ic_topcoder
+    Contest.Platform.CODEFORCES -> R.drawable.ic_codeforces
+    Contest.Platform.CODECHEF -> R.drawable.ic_codechef
+    Contest.Platform.CODEFORCES_GYM -> R.drawable.ic_codeforcesgym
+    Contest.Platform.LEETCODE -> R.drawable.ic_leetcode
+    Contest.Platform.KICK_START -> R.drawable.ic_kickstart
+    Contest.Platform.HACKEREARTH -> R.drawable.ic_hackerearth
+    Contest.Platform.HACKERRANK -> R.drawable.ic_hackerrank
+    Contest.Platform.CS_ACADEMY -> R.drawable.ic_csacademy
+    Contest.Platform.TOPH -> R.drawable.ic_toph
+}
+
 @Composable
-@Preview
-private fun Preview() = ContentView()
+private fun getDateTime(seconds: Long): String {
+    val dateFormat =
+        SimpleDateFormat(stringResource(R.string.contest_date_format), Locale.getDefault())
+    return dateFormat.format(Date(seconds)).toString()
+}
