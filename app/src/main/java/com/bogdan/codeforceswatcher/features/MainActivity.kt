15cf492fbc091ee33ada8c7e0265a6594787259d
Oleg Derevyanko <trekol39042@gmail.com>
2022-03-24 23:11:50 +0200

    #414; Code update.

--- app/src/main/java/com/bogdan/codeforceswatcher/features/MainActivity.kt
+++ app/src/main/java/com/bogdan/codeforceswatcher/features/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.bogdan.codeforceswatcher.features
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import android.graphics.PorterDuff
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.core.view.children
 import androidx.fragment.app.Fragment
 import com.bogdan.codeforceswatcher.R
 import com.bogdan.codeforceswatcher.components.AddUserBottomSheet
@@ -21,7 +17,6 @@ import com.bogdan.codeforceswatcher.features.problems.ProblemsFragment
 import com.bogdan.codeforceswatcher.features.users.UsersFragment
 import com.bogdan.codeforceswatcher.util.FeedbackController
 import com.google.android.material.bottomnavigation.BottomNavigationMenuView
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import io.xorum.codeforceswatcher.features.problems.redux.ProblemsActions
 import io.xorum.codeforceswatcher.redux.analyticsController
 import io.xorum.codeforceswatcher.redux.store
@@ -40,13 +35,29 @@ class MainActivity : AppCompatActivity() {
         initViews()
     }
 
-    private fun updateFragment(fragment: Fragment) {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.fragment_container, fragment)
-            .commit()
+    private fun initViews() {
+        bottom_navigation.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.navigation_contests -> { onContestsTabSelected(); true }
+                R.id.navigation_users -> { onUsersTabSelected(); true }
+                R.id.navigation_news -> { onNewsTabSelected(); true }
+                R.id.navigation_problems -> { onProblemsTabSelected(); true }
+                else -> false
+            }
+        }
+
+        onContestsTabSelected()
+
+        val menuView = bottom_navigation.getChildAt(0) as? BottomNavigationMenuView ?: return
+        for (i in 0 until menuView.childCount) {
+            val activeLabel = menuView.getChildAt(i).findViewById<View>(R.id.largeLabel)
+            if (activeLabel is TextView) {
+                activeLabel.setPadding(0, 0, 0, 0)
+            }
+        }
     }
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     private fun onContestsTabSelected() {
         updateFragment(ContestsFragment())
 
@@ -62,6 +73,7 @@ class MainActivity : AppCompatActivity() {
         fab.setImageDrawable(getDrawable(R.drawable.ic_eye))
     }
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     private fun onUsersTabSelected() {
         updateFragment(UsersFragment())
 
@@ -71,6 +83,7 @@ class MainActivity : AppCompatActivity() {
         fab.setImageDrawable(getDrawable(R.drawable.ic_plus))
     }
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     private fun onNewsTabSelected() {
         updateFragment(NewsFragment())
 
@@ -95,6 +108,13 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun updateFragment(fragment: Fragment) =
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+
+    @SuppressLint("UseCompatLoadingForDrawables")
     private fun updateProblemsFAB(problemsIsFavourite: Boolean) =
         if (problemsIsFavourite) {
             fab.setImageDrawable(getDrawable(R.drawable.ic_all))
@@ -120,41 +140,6 @@ class MainActivity : AppCompatActivity() {
         type = "text/plain"
         putExtra(Intent.EXTRA_TEXT, getString(R.string.share_cw_message))
     })
-
-    private fun initViews() {
-        bottom_navigation.setOnNavigationItemSelectedListener { item ->
-            when(item.itemId) {
-                R.id.navigation_contests -> {
-                    onContestsTabSelected()
-                    true
-                }
-                R.id.navigation_users -> {
-                    onUsersTabSelected()
-                    true
-                }
-                R.id.navigation_news -> {
-                    onNewsTabSelected()
-                    true
-                }
-                R.id.navigation_problems -> {
-                    onProblemsTabSelected()
-                    true
-                }
-                else -> false
-            }
-        }
-
-        onContestsTabSelected()
-
-        val menuView = bottom_navigation.getChildAt(0) as? BottomNavigationMenuView ?: return
-
-        for (i in 0 until menuView.childCount) {
-            val activeLabel = menuView.getChildAt(i).findViewById<View>(R.id.largeLabel)
-            if (activeLabel is TextView) {
-                activeLabel.setPadding(0, 0, 0, 0)
-            }
-        }
-    }
 
     companion object {
         private const val CONTESTS_LINK = "https://clist.by/"
